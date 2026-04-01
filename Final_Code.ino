97dbfd38b889491529547eae5c6f86bac87078db
PenguinHater <gtelegar@mtu.edu>
2026-04-01 18:48:36 -0400

    Pressure only Final_Code.ino

--- Final_Code.ino
+++ Final_Code.ino
@@ -108,10 +108,12 @@ void setup(void){
   bool adxlOK = initADXL375();
   bool icmOK  = initICM20948();
 
+  /*
   if(!adxlOK || !icmOK){
     Serial.println("Sensor initialization failed!");
     while(1){} // Stop program if sensors fail
   }
+  */
 
   Serial.println("All sensors initialized. Waiting for launch...");
 }
@@ -126,6 +128,7 @@ void loop() {
   rocketStatus();
 
   // 2. CHECK FOR LAUNCH (Only if we haven't launched yet)
+  /*
   if (!hasLaunched) {
     adxl.getEvent(&accel);
     vertAccel = accel.acceleration.x;
@@ -141,6 +144,7 @@ void loop() {
       Serial.println("NOT LAUNCHED...");
     }
   }
+  */
 
   // Delay to prevent spamming the Serial Monitor too fast
   // 5 seconds might be too slow to catch apogee exactly, consider lowering this to 500ms or 1000ms later!
@@ -153,23 +157,28 @@ Checks to make sure the rocket flight is nominal and gets current data
 */
 void rocketStatus() {
   
+  /*
   // Check for apogee ONLY if the rocket has actually launched
   if(hasLaunched && (millis() - launchTime >= appogeTime)){ 
     stop();
   }
+  */
   
+  /*
   float ax375, ay375, az375;
   readADXL375(ax375, ay375, az375);
   readICM20948(); 
+  */
 
   // --- READ PRESSURE ---
   readPressure();
 
+  /*
   Serial.printf("ADXL375: X=%.2f, Y=%.2f, Z=%.2f\n", ax375, ay375, az375);
   Serial.printf("ICM20948: AX=%.2f, AY=%.2f, AZ=%.2f | GX=%.2f, GY=%.2f, GZ=%.2f\n",
                 icm_accel.acceleration.x, icm_accel.acceleration.y, icm_accel.acceleration.z, 
                 icm_gyro.gyro.x, icm_gyro.gyro.y, icm_gyro.gyro.z);
-                
+
   // --- PRINT PRESSURE ---
   Serial.printf("Pressure: %.2f\n", currentPressure); 
 
@@ -231,6 +240,7 @@ void rocketStatus() {
           stop();
       }
   }
+  */
 }
 
 /*
@@ -260,16 +270,138 @@ void readADXL375(float &x, float &y, float &z) {
 }
 
 bool initICM20948() {
+   Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit ICM20948 test!");
+
+  // Try to initialize!
   if (!icm.begin_I2C()) {
-    Serial.println("Failed to find ICM20948 chip");
-    return false; 
+    // if (!icm.begin_SPI(ICM_CS)) {
+    // if (!icm.begin_SPI(ICM_CS, ICM_SCK, ICM_MISO, ICM_MOSI)) {
+
+    Serial.println("ICM20948 not found!");
+    return false;
   }
   Serial.println("ICM20948 Found!");
+  // icm.setAccelRange(ICM20948_ACCEL_RANGE_16_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (icm.getAccelRange()) {
+  case ICM20948_ACCEL_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case ICM20948_ACCEL_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case ICM20948_ACCEL_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case ICM20948_ACCEL_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  Serial.println("OK");
+
+  // icm.setGyroRange(ICM20948_GYRO_RANGE_2000_DPS);
+  Serial.print("Gyro range set to: ");
+  switch (icm.getGyroRange()) {
+  case ICM20948_GYRO_RANGE_250_DPS:
+    Serial.println("250 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_500_DPS:
+    Serial.println("500 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_1000_DPS:
+    Serial.println("1000 degrees/s");
+    break;
+  case ICM20948_GYRO_RANGE_2000_DPS:
+    Serial.println("2000 degrees/s");
+    break;
+  }
+
+  //  icm.setAccelRateDivisor(4095);
+  uint16_t accel_divisor = icm.getAccelRateDivisor();
+  float accel_rate = 1125 / (1.0 + accel_divisor);
+
+  Serial.print("Accelerometer data rate divisor set to: ");
+  Serial.println(accel_divisor);
+  Serial.print("Accelerometer data rate (Hz) is approximately: ");
+  Serial.println(accel_rate);
+
+  //  icm.setGyroRateDivisor(255);
+  uint8_t gyro_divisor = icm.getGyroRateDivisor();
+  float gyro_rate = 1100 / (1.0 + gyro_divisor);
+
+  Serial.print("Gyro data rate divisor set to: ");
+  Serial.println(gyro_divisor);
+  Serial.print("Gyro data rate (Hz) is approximately: ");
+  Serial.println(gyro_rate);
+
+  // icm.setMagDataRate(AK09916_MAG_DATARATE_10_HZ);
+  Serial.print("Magnetometer data rate set to: ");
+  switch (icm.getMagDataRate()) {
+  case AK09916_MAG_DATARATE_SHUTDOWN:
+    Serial.println("Shutdown");
+    break;
+  case AK09916_MAG_DATARATE_SINGLE:
+    Serial.println("Single/One shot");
+    break;
+  case AK09916_MAG_DATARATE_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case AK09916_MAG_DATARATE_20_HZ:
+    Serial.println("20 Hz");
+    break;
+  case AK09916_MAG_DATARATE_50_HZ:
+    Serial.println("50 Hz");
+    break;
+  case AK09916_MAG_DATARATE_100_HZ:
+    Serial.println("100 Hz");
+    break;
+  }
+  Serial.println();
   return true;
 }
 
 void readICM20948() {
-  icm.getEvent(&icm_accel, &icm_gyro, &icm_temp, &icm_mag);
+   //  /* Get a new normalized sensor event */
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t mag;
+  sensors_event_t temp;
+  icm.getEvent(&accel, &gyro, &temp, &mag);
+
+  Serial.print("\t\tTemperature ");
+  Serial.print(temp.temperature);
+  Serial.println(" deg C");
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tAccel X: ");
+  Serial.print(accel.acceleration.x);
+  Serial.print(" \tY: ");
+  Serial.print(accel.acceleration.y);
+  Serial.print(" \tZ: ");
+  Serial.print(accel.acceleration.z);
+  Serial.println(" m/s^2 ");
+
+  Serial.print("\t\tMag X: ");
+  Serial.print(mag.magnetic.x);
+  Serial.print(" \tY: ");
+  Serial.print(mag.magnetic.y);
+  Serial.print(" \tZ: ");
+  Serial.print(mag.magnetic.z);
+  Serial.println(" uT");
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tGyro X: ");
+  Serial.print(gyro.gyro.x);
+  Serial.print(" \tY: ");
+  Serial.print(gyro.gyro.y);
+  Serial.print(" \tZ: ");
+  Serial.print(gyro.gyro.z);
+  Serial.println(" radians/s ");
+  Serial.println();
 }
 
 // --- PRESSURE SENSOR READ & CONVERT FUNCTION ---
